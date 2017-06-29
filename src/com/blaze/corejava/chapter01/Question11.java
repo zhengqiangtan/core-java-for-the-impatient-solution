996607c8bead12fc0e0ec5b1f8885e8b5e1bb8c6
SherlockBlaze <sherlockblaze@gmail.com>
2017-06-30 02:16:10 +0800

    Question 7\10\11\12 Update

--- src/com/blaze/corejava/chapter01/Question11.java
+++ src/com/blaze/corejava/chapter01/Question11.java
@@ -7,11 +7,21 @@ import java.io.InputStreamReader;
 /**
  * Created by sherlockblaze on 30/06/2017.
  * 读取一个字符串，输出所有的非ASCII字符，以及它们的Unicode值
+ * <p>
+ * 中文很6liu
+ * 4e2d
+ * 6587
+ * 5f88
+ * 36
+ * 6c
+ * 69
+ * 75
  */
 public class Question11 {
-    public static void main(String[] args)throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
         String readStr = buffer.readLine();
-        
+        for (int i = 0; i < readStr.length(); i++)
+            System.out.println(Integer.toHexString(readStr.charAt(i)));
     }
 }
---
+++ src/com/blaze/corejava/chapter01/Question12.java
@@ -0,0 +1,7 @@
+package com.blaze.corejava.chapter01;
+
+/**
+ * Created by sherlockblaze on 30/06/2017.
+ */
+public class Question12 {
+}
